11e9c4d694b13746b07641502bff8d1471017713
Your Name <you@example.com>
2018-09-15 16:48:48 +0800

    commit our completed code examples

---
+++ lab2-completed/flow-complete.js
@@ -0,0 +1,58 @@
+//lab 2 -- program flow in JavaScript
+
+/* const canPaint = false;
+const isCat = false;
+const isBear = false;
+
+if(canPaint && isCat) {
+    console.log(`I think we should name him Bob Ross the Cat!`);
+} else if(isCat) {
+    console.log(`Since he can't paint let us name him John`);
+} else if(canPaint || isBear) {
+    console.log(`That could be a bear, or it could be a man, but boy can it paint!`);
+} else {
+    console.log(`I have no idea what that thing is!`);
+}
+
+*/
+
+//switch statement
+/* const catName = `Christian`;
+
+switch(catName) {
+    case `Mr Bigglesworth`: console.log(catName + "do you have any sharks?");
+    break;
+    case `Chaos`: console.log(`warning: do not allow ${catName} near the plants!`);
+    break;
+    case `Bob Ross`: console.log(`oh that's just ${catName} painting a happy little accident`);
+    break;
+    default: console.log(`I'm sorry, I don't know ${catName}`);
+}
+*/
+
+//While statement - count to 5
+/* let counter = 0;
+while(counter < 6) {
+    console.log(counter);
+    counter = counter + 1;
+}
+*/
+
+//do loop
+/* let authorized = false;
+
+do {
+    //we want to prompt for a password and see if it's equal to 'hunter2', then if so, set authorized equal to true.
+    if(prompt(`please enter a password`) === `hunter2`) {
+        console.log(`super secret message here!`);
+        authorized = true;
+    } 
+} while (!authorized);
+*/
+
+//the for loop
+//for(let i=0;i<6;i++) {
+//   console.log(i);
+//}
+
+//the end
