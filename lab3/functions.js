68dd252ae9f9c2447965d12d09526d28f26d1067
Christian <christian@nyu.edu>
2018-09-18 20:58:11 +0800

    lab readme update

--- lab3/functions.js
+++ lab3/functions.js
@@ -1,1 +1,11 @@
 //functions, functions(), => functions
+
+const power = function(base, exponent) {
+    let result = 1;
+    for (let i=0; i < exponent; i++) {
+      result *= base;
+    }
+    return result;
+  };
+  console.log(power(2, 10));
+  //1024
