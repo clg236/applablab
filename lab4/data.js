34d62e9cebc91da07fa0c4159110629d33c26f89
xu.li <athenalightenedmypath@gmail.com>
2018-10-09 09:33:31 +0800

    a few examples

--- lab4/data.js
+++ lab4/data.js
@@ -1,1 +1,28 @@
-//we will add code here soon!
+let cats = [
+    {
+        name: "Tabby",
+        lovescatnip: true
+    },
+    {
+        name: "Bob Ross",
+        lovescatnip: false
+    },
+    {
+        name: "Peaches",
+        lovescatnip: true
+    }
+];
+
+/**
+ * Check if a cat loves catnip
+ * @param {string} name 
+ */
+function lovesCatnip(name) {
+    let cat = cats.filter(function (item) {
+        return item.name == name;
+    });
+
+    return cat[0].lovescatnip;
+}
+
+console.log(lovesCatnip('Tabby'));
